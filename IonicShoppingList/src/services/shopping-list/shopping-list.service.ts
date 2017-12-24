ad8345d2a1eb931b83ba3ebd03d651996fcd43cf
Patrick Vianna <patrickviannapblv@gmail.com>
2017-12-24 17:03:03 -0200

    Remove Shopping List

--- IonicShoppingList/src/services/shopping-list/shopping-list.service.ts
+++ IonicShoppingList/src/services/shopping-list/shopping-list.service.ts
@@ -21,4 +21,8 @@ export class ShoppingListService {
   editItem(item: Item){
     return this.shoppingListRef.update(item.key, item);
   }
+
+  removeItem(item: Item){
+    return this.shoppingListRef.remove(item.key);
+  }
 }
